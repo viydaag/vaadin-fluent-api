962b5375244306a5319d0b76c9f41427e0f89518
Jean-Christophe Fortier <jean-christophe.fortier@desjardins.com>
2017-08-30 15:01:37 -0400

    fix FluentAbstractField

--- src/main/java/com/vaadin/fluent/api/FluentAbstractField.java
+++ src/main/java/com/vaadin/fluent/api/FluentAbstractField.java
@@ -4,6 +4,6 @@ import com.vaadin.fluent.api.FluentComponent.FluentFocusable;
 
 
 public interface FluentAbstractField<THIS extends FluentAbstractField<THIS, FIELD>, FIELD>
-        extends FluentComponent<THIS>, FluentHasValue<THIS, FIELD>, FluentFocusable<THIS> {
+        extends FluentAbstractComponent<THIS>, FluentHasValue<THIS, FIELD>, FluentFocusable<THIS> {
 
 }
