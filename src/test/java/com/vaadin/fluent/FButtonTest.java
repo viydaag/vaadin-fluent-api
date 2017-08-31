cc4caf15b98f9ffb43d38b5cf1b53320b42d983e
Jean-Christophe Fortier <jean-christophe.fortier@desjardins.com>
2017-08-31 11:19:11 -0400

    add FNativeButton

--- src/test/java/com/vaadin/fluent/FButtonTest.java
+++ src/test/java/com/vaadin/fluent/FButtonTest.java
@@ -9,13 +9,14 @@ import com.vaadin.event.FieldEvents.BlurEvent;
 import com.vaadin.event.FieldEvents.FocusEvent;
 import com.vaadin.event.ShortcutAction;
 import com.vaadin.fluent.ui.FButton;
+import com.vaadin.fluent.ui.FNativeButton;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.ui.Button.ClickEvent;
 
 public class FButtonTest {
 
     @Test
-    public void test() {
+    public void testButton() {
         FButton button = new FButton().withCaption("My button")
                                       .withIcon(VaadinIcons.ARROW_BACKWARD)
                                       .withIconAlternateText("backward")
@@ -34,6 +35,29 @@ public class FButtonTest {
         assertEquals(1, button.getListeners(FocusEvent.class).size());
         assertEquals(1, button.getListeners(BlurEvent.class).size());
         assertTrue(button.getClickShortcut() != null);
+        assertTrue(button.isDisableOnClick());
+
+    }
+
+    @Test
+    public void testNativeButton() {
+        FNativeButton button = new FNativeButton().withCaption("My button")
+                                                  .withIcon(VaadinIcons.ARROW_BACKWARD)
+                                                  .withIconAlternateText("backward")
+                                                  .withClickListener(click -> System.out.println("clicked"))
+                                                  .withClickShortcut(ShortcutAction.KeyCode.ENTER, null)
+                                                  .withBlurListener(blur -> System.out.println("lost focus"))
+                                                  .withFocusListener(focus -> System.out.println("gain focus"))
+                                                  .withTabIndex(5)
+                                                  .withDisableOnClick(true);
+
+        assertEquals("My button", button.getCaption());
+        assertEquals(VaadinIcons.ARROW_BACKWARD, button.getIcon());
+        assertEquals("backward", button.getIconAlternateText());
+        assertEquals(5, button.getTabIndex());
+        assertEquals(1, button.getListeners(ClickEvent.class).size());
+        assertEquals(1, button.getListeners(FocusEvent.class).size());
+        assertEquals(1, button.getListeners(BlurEvent.class).size());
         assertTrue(button.isDisableOnClick());
 
     }
